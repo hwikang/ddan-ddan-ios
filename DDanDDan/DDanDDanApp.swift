29d5ea68a89848ce7232da7c4789a7e517befa19
강휘 <kh@paytalab.com>
2024-07-13 21:39:14 +0900

    온보딩 view 구현

---
+++ DDanDDan/DDanDDanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDanDDanApp.swift
+//  DDanDDan
+//
+//  Created by paytalab on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct DDanDDanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
