68e62fcc796e9b68732317a3caa20d927bcf0bda
Zoe <mam07065@naver.com>
2024-09-05 21:49:33 +0900

    폰트 세팅

--- DDanDDan/Resource/Font.swift
+++ DDanDDan/Resource/Font.swift
@@ -1,8 +1,31 @@
 //
-//  Fonts.swift
+//  Font.swift
 //  DDanDDan
 //
 //  Created by 이지희 on 9/5/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension Font {
+  ///neoDunggeunmo
+  static let neoDunggeunmo14: Font = .custom("NeoDunggeunmo", size: 14)
+  static let neoDunggeunmo22: Font = .custom("NeoDunggeunmo", size: 22)
+  static let neoDunggeunmo24: Font = .custom("NeoDunggeunmo", size: 24)
+  static let neoDunggeunmo42: Font = .custom("NeoDunggeunmo", size: 42)
+  static let neoDunggeunmo52: Font = .custom("NeoDunggeunmo", size: 52)
+  
+  /// heading
+  static let heading3_bold24: Font = .custom("Pretendard-Bold", size: 24)
+  static let heading5_semibold18: Font = .custom("Pretendard-SemiBold", size: 18)
+  static let heading6_semibold16: Font = .custom("Pretendard-SemiBold", size: 16)
+  static let heading7_medium16: Font = .custom("Pretendard-Medium", size: 16)
+  
+  /// subTitle
+  static let subTitle1_semibold16: Font = .custom("Pretendard-SemiBold", size: 14)
+  
+  /// body
+  static let body1_regular16: Font = .custom("Pretendard-Regular", size: 16)
+  static let body2_regular14: Font = .custom("Pretendard-Regular", size: 14)
+  static let body3_regular12: Font = .custom("Pretendard-Regular", size: 12)
+}
