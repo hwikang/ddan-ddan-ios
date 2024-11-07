d044a168c75f90d42868c2f70c246d2dd7a26a13
Zoe <mam07065@naver.com>
2024-11-08 07:05:39 +0900

    펫 보관함 뷰 로직 구현

--- DDanDDan/Presenter/Home/Views/Archive/PetArchiveView.swift
+++ DDanDDan/Presenter/Home/Views/Archive/PetArchiveView.swift
@@ -8,7 +8,16 @@
 import SwiftUI
 
 struct PetArchiveView: View {
-    @Environment(\.dismiss) private var dismiss
+    @ObservedObject var coordinator: AppCoordinator
+    @StateObject var viewModel: PetArchiveViewModel
+    
+    @State private var selectedIndex: Int? = nil
+    @State private var showToast = false
+    
+    init(viewModel: PetArchiveViewModel, coordinator: AppCoordinator) {
+        self.coordinator = coordinator
+        self._viewModel = StateObject(wrappedValue: viewModel)
+    }
     
     let columns = [
         GridItem(.flexible()),
@@ -24,7 +33,7 @@ struct PetArchiveView: View {
             VStack {
                 HStack {
                     Button {
-                        dismiss()
+                        coordinator.pop()
                     } label: {
                         Image(.arrow)
                     }
@@ -50,11 +59,30 @@ struct PetArchiveView: View {
                 HStack {
                     LazyVGrid(columns: columns, spacing: 20) {
                         ForEach(0..<9, id: \.self) { index in
-                            Rectangle()
-                                .fill(Color.gray)
-                                .overlay(Text("\(index + 1)").foregroundColor(.white))
-                                .aspectRatio(1, contentMode: .fit)
-                                .padding(.horizontal, 8)
+                            ZStack {
+                                RoundedRectangle(cornerSize: CGSize(width: 8, height: 8))
+                                    .stroke(selectedIndex == index ? Color.buttonGreen : Color.clear, lineWidth: 4)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 8).foregroundColor(.borderGray) // 배경색 적용
+                                    )
+                                    .frame(maxWidth: 100, maxHeight: 100)
+                                
+                                if index < viewModel.PetList.count {
+                                    viewModel.characterImage(petType: viewModel.PetList[index].type, level: viewModel.PetList[index].level)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: 100, maxHeight: 100)
+                                } else {
+                                    Image(.questionMark)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 48, height: 48)
+                                        .padding(24)
+                                }
+                            }
+                            .onTapGesture {
+                                toggleSelection(for: index)
+                            }
                         }
                     }
                 }
@@ -62,15 +90,49 @@ struct PetArchiveView: View {
                 Spacer()
                 GreenButton(action: {
                     print("선택 완료")
+                    showToastMessage()
                 }, title: "선택 완료", disabled: .constant(false))
                 .padding(.bottom, 44)
             }
-            .navigationBarHidden(true)
+            if showToast {
+                VStack {
+                    ToastView(message: "새로운 펫을 준비중이에요")
+                        .onTapGesture {
+                            hideToastMessage() // 탭하면 토스트 사라짐
+                        }
+                }
+                .transition(.asymmetric(
+                    insertion: .move(edge: .top).combined(with: .opacity),
+                    removal: .opacity)) // 사라질 때는 페이드 아웃만
+                .animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3), value: showToast)
+                .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height - 250)
+            }
+        }
+        .navigationBarHidden(true)
+    }
+    
+
+    private func toggleSelection(for index: Int) {
+        if selectedIndex == index {
+            selectedIndex = nil // 이미 선택된 경우 선택 해제
+        } else {
+            selectedIndex = index // 새로운 인덱스를 선택
         }
     }
+    
+    private func showToastMessage() {
+            showToast = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                hideToastMessage()
+            }
+        }
+        
+        private func hideToastMessage() {
+            showToast = false
+        }
 }
 
 
 #Preview {
-    PetArchiveView()
+    PetArchiveView(viewModel: PetArchiveViewModel(repository: HomeRepository()), coordinator: AppCoordinator())
 }
