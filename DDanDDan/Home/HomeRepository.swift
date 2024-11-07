632b34f7387a170388142679192a72afa2e9bea6
Zoe <mam07065@naver.com>
2024-11-07 19:27:55 +0900

    Home Feature Repository 생성

---
+++ DDanDDan/Home/HomeRepository.swift
@@ -0,0 +1,144 @@
+//
+//  HomeRepository.swift
+//  DDanDDan
+//
+//  Created by 이지희 on 11/7/24.
+//
+
+import Foundation
+
+protocol HomeRepositoryProtocol {
+    // MARK: GET Method
+    func getMainPetInfo() async -> Result<MainPet, NetworkError>
+    func getPetArchive() async -> Result<PetArchiveModel, NetworkError>
+    func getSpecificPet(petId: String) async -> Result<Pet, NetworkError>
+    
+    // MARK: POST Method
+    func updateMainPet(petId: String) async -> Result<MainPet, NetworkError>
+    func feedPet(petId: String) async -> Result<DailyInfo, NetworkError>
+    func playPet(petId: String) async -> Result<DailyInfo, NetworkError>
+    func addNewPet(petType: PetType) async -> Result<Pet, NetworkError>
+    func addNewRandomPet() async -> Result<Pet, NetworkError>
+    
+    // MARK: PATCH Method
+    func updateDailyKcal(calorie: Int) async -> Result<DailyUserData, NetworkError>
+}
+
+
+public struct HomeRepository: HomeRepositoryProtocol {
+    
+    private let userNetwork = UserNetwork()
+    private let petNetwork = PetsNetwork()
+    
+    // MARK: - GET
+    
+    public func getMainPetInfo() async -> Result<MainPet, NetworkError> {
+        guard let accessToken = await UserManager.shared.accessToken else { return .failure(.requestFailed("Access Token Nil"))}
+        
+        let result = await userNetwork.fetchUserMainPet(accessToken: accessToken)
+        
+        if case .success(let userData) = result {
+            
+        }
+        
+        return result
+    }
+    
+    public func getPetArchive() async -> Result<PetArchiveModel, NetworkError> {
+        guard let accessToken = await UserManager.shared.accessToken else { return .failure(.requestFailed("Access Token Nil"))}
+        
+        let result = await petNetwork.fetchPetArchieve(accessToken: accessToken)
+        
+        if case .success(let userData) = result {
+            
+        }
+        
+        return result
+    }
+    
+    public func getSpecificPet(petId: String) async -> Result<Pet, NetworkError> {
+        guard let accessToken = await UserManager.shared.accessToken else { return .failure(.requestFailed("Access Token Nil"))}
+        
+        let result = await petNetwork.fetchSpecificPet(accessToken: accessToken, petId: petId)
+        
+        if case .success(let userData) = result {
+            
+        }
+        
+        return result
+    }
+    
+    // MARK: - POST
+    
+    public func updateMainPet(petId: String) async -> Result<MainPet, NetworkError> {
+        guard let accessToken = await UserManager.shared.accessToken else { return .failure(.requestFailed("Access Token Nil"))}
+        
+        let result = await userNetwork.setMainPet(accessToken: accessToken, petID: petId)
+        
+        if case .success(let userData) = result {
+            
+        }
+        
+        return result
+    }
+    
+    public func feedPet(petId: String) async -> Result<DailyInfo, NetworkError> {
+        guard let accessToken = await UserManager.shared.accessToken else { return .failure(.requestFailed("Access Token Nil"))}
+        
+        let result = await petNetwork.postPetFeed(accessToken: accessToken, petId: petId)
+        
+        if case .success(let userData) = result {
+            
+        }
+        
+        return result
+    }
+    
+    public func playPet(petId: String) async -> Result<DailyInfo, NetworkError> {
+        guard let accessToken = await UserManager.shared.accessToken else { return .failure(.requestFailed("Access Token Nil"))}
+        
+        let result = await petNetwork.postPetPlay(accessToken: accessToken, petId: petId)
+        
+        if case .success(let userData) = result {
+            
+        }
+        
+        return result
+    }
+    
+    public func addNewPet(petType: PetType) async -> Result<Pet, NetworkError> {
+        guard let accessToken = await UserManager.shared.accessToken else { return .failure(.requestFailed("Access Token Nil"))}
+        
+        let result = await petNetwork.addPet(accessToken: accessToken, petType: petType)
+        
+        if case .success(let userData) = result {
+            
+        }
+        
+        return result
+    }
+    
+    public func addNewRandomPet() async -> Result<Pet, NetworkError> {
+        guard let accessToken = await UserManager.shared.accessToken else { return .failure(.requestFailed("Access Token Nil"))}
+        
+        let result = await petNetwork.addRandomPet(accessToken: accessToken)
+        
+        if case .success(let userData) = result {
+            
+        }
+        
+        return result
+    }
+    
+    public func updateDailyKcal(calorie: Int) async -> Result<DailyUserData, NetworkError> {
+        guard let accessToken = await UserManager.shared.accessToken else { return .failure(.requestFailed("Access Token Nil"))}
+        
+        let result = await userNetwork.patchDailyKcal(accessToken: accessToken, calorie: calorie)
+        
+        if case .success(let userData) = result {
+            
+        }
+        
+        return result
+    }
+}
