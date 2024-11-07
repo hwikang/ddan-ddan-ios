2dee476781494ea940afc38dfab5198b7005b21a
Zoe <mam07065@naver.com>
2024-11-07 19:27:04 +0900

    User 도메인 API 추가 연결

--- DDanDDan/Network/UserNetwork.swift
+++ DDanDDan/Network/UserNetwork.swift
@@ -10,8 +10,36 @@ import Alamofire
 public struct UserNetwork {
     private let manager = NetworkManager()
     
-    public func update(accessToken: String,
-                       name: String?, purposeCalorie: Int?) async -> Result<UserData, NetworkError> {
+    // MARK: - GET
+    
+    public func fetchUserMainPet(accessToken: String) async -> Result<MainPet, NetworkError> {
+        let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
+        
+        return await manager.request(
+            url: PathString.User.mainPet,
+            method: .get,
+            headers: headers
+        )
+    }
+    
+    public func fetchUserInfo(accessToken: String) async -> Result<UserData, NetworkError> {
+        let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
+        
+        return await manager.request(
+            url: PathString.User.user,
+            method: .get,
+            headers: headers
+        )
+    }
+    
+    
+    // MARK: - PUT
+    
+    public func update(
+        accessToken: String,
+        name: String?,
+        purposeCalorie: Int?
+    ) async -> Result<UserData, NetworkError> {
         var parameter: Parameters = [:]
         if let name = name {
             parameter["name"] = name
@@ -20,16 +48,40 @@ public struct UserNetwork {
             parameter["purposeCalorie"] = purposeCalorie
         }
         let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
-        return await manager.request(url: "/v1/user/me", method: .put, headers: headers,
-                                     parameters: parameter, encoding: JSONEncoding.default)
+        return await manager.request(
+            url: PathString.User.user,
+            method: .put,
+            headers: headers,
+            parameters: parameter,
+            encoding: JSONEncoding.default
+        )
     }
+    
+    // MARK: - PATCH
+    
+    public func patchDailyKcal(accessToken: String, calorie: Int) async -> Result<DailyUserData, NetworkError> {
+        let parameter: Parameters = ["calorie": calorie]
+        let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
+        return await manager.request(
+            url: PathString.User.updateDailyKcal,
+            method: .patch,
+            headers: headers,
+            parameters: parameter,
+            encoding: JSONEncoding.default
+            )
+    }
+    
+    // MARK: - POST
     
     public func setMainPet(accessToken: String, petID: String) async -> Result<MainPet, NetworkError> {
         let parameter: Parameters = ["petId": petID]
         let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
-        return await manager.request(url: "/v1/user/me/main-pet", method: .post, headers: headers,
-                                     parameters: parameter, encoding: JSONEncoding.default)
-
-
+        return await manager.request(
+            url: PathString.User.mainPet,
+            method: .post,
+            headers: headers,
+            parameters: parameter,
+            encoding: JSONEncoding.default
+        )
     }
 }
