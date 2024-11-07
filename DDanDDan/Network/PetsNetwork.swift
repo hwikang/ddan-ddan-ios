c31af353a2a8057cc069eae93ea0fdf960fd6ec2
Zoe <mam07065@naver.com>
2024-11-07 19:26:34 +0900

    Pet 도메인 API 연결

--- DDanDDan/Network/PetsNetwork.swift
+++ DDanDDan/Network/PetsNetwork.swift
@@ -12,12 +12,81 @@ import Alamofire
 public struct PetsNetwork {
     private let manager = NetworkManager()
     
+    // MARK: - GET
+    
+    public func fetchSpecificPet(accessToken: String, petId: String) async -> Result<Pet, NetworkError> {
+        let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
+        
+        return await manager.request(
+            url: PathString.Pet.fetchPet + petId,
+            method: .get,
+            headers: headers
+        )
+    }
+    
+    public func fetchPetArchieve(accessToken: String) async -> Result<PetArchiveModel, NetworkError> {
+        let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
+        
+        return await manager.request(
+            url: PathString.Pet.userPets,
+            method: .get,
+            headers: headers
+        )
+    }
+    
+    // MARK: - POST
+    
     public func addPet(accessToken: String, petType: PetType) async -> Result<Pet, NetworkError> {
         let parameter: Parameters = [
             "petType": petType.rawValue
         ]
         let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
-
-        return await manager.request(url: "/v1/pets/me", method: .post, headers: headers, parameters: parameter, encoding: JSONEncoding.default)
+        
+        return await manager.request(
+            url: PathString.Pet.userPets,
+            method: .post,
+            headers: headers,
+            parameters: parameter,
+            encoding: JSONEncoding.default
+        )
+    }
+    
+    public func addRandomPet(accessToken: String) async -> Result<Pet, NetworkError> {
+        let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
+        
+        return await manager.request(
+            url: PathString.Pet.randomPet,
+            method: .post,
+            headers: headers
+        )
+    }
+    
+    public func postPetFeed(accessToken: String, petId: String) async -> Result<DailyInfo, NetworkError> {
+        let parameter: Parameters = [
+            "petId": petId
+        ]
+        let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
+        
+        return await manager.request(
+            url: PathString.Pet.userPets + petId + "/food",
+            method: .post, headers: headers,
+            parameters: parameter,
+            encoding: JSONEncoding.default
+        )
+    }
+    
+    public func postPetPlay(accessToken: String, petId: String) async -> Result<DailyInfo, NetworkError> {
+        let parameter: Parameters = [
+            "petId": petId
+        ]
+        let headers: HTTPHeaders = ["Authorization": "Bearer " + accessToken]
+        
+        return await manager.request(
+            url: PathString.Pet.userPets + petId + "/play",
+            method: .post, headers: headers,
+            parameters: parameter,
+            encoding: JSONEncoding.default
+        )
     }
 }
+
