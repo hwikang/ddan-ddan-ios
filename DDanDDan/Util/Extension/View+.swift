8815be0f92d1ae22b991502a2f0ad9d26eaf77a2
Zoe <mam07065@naver.com>
2024-11-08 17:50:59 +0900

    폴더링 일부 변경 및 뷰 익스텐션 추가

---
+++ DDanDDan/Util/Extension/View+.swift
@@ -0,0 +1,32 @@
+//
+//  View+.swift
+//  DDanDDan
+//
+//  Created by 이지희 on 11/8/24.
+//
+
+import UIKit
+import SwiftUI
+
+extension View {
+    func transparentFullScreenCover<Content: View>(isPresented: Binding<Bool>, content: @escaping () -> Content) -> some View {
+        fullScreenCover(isPresented: isPresented) {
+            ZStack {
+                content()
+            }
+            .background(TransparentBackground())
+        }
+    }
+}
+
+struct TransparentBackground: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView()
+        DispatchQueue.main.async {
+            view.superview?.superview?.backgroundColor = .clear
+        }
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView, context: Context) {}
+}
