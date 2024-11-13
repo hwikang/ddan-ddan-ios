0b4f6960a77520a949e8cf52e2e160453baba932
Zoe <mam07065@naver.com>
2024-11-14 02:24:24 +0900

    HomeViewModel 간소화를 위한 enum 수정

--- DDanDDan/Presenter/Home/HomeViewModel.swift
+++ DDanDDan/Presenter/Home/HomeViewModel.swift
@@ -54,6 +54,14 @@ final class HomeViewModel: ObservableObject {
             UserDefaultValue.petType = petData.mainPet.type.rawValue
             UserDefaultValue.petId = petData.mainPet.id
             
+            let watchData = WatchPetModel.init(
+                petType: petData.mainPet.type,
+                goalKcal: userData.purposeCalorie,
+                level: petData.mainPet.level
+            )
+            
+            WatchConnectivityManager.shared.sendMessage(message: ["watchPet" : watchData])
+            
             await updateGoalStatus()
         }
     }
@@ -144,64 +152,12 @@ final class HomeViewModel: ObservableObject {
     
     /// 캐릭터에 맞는 배경 이미지
     func backgroundImage() -> Image {
-        switch homePetModel.petType {
-        case .pinkCat:
-            return Image(.pinkBackground).resizable()
-        case .greenHam:
-            return Image(.greenBackground).resizable()
-        case .purpleDog:
-            return Image(.purpleBackground).resizable()
-        case .bluePenguin:
-            return Image(.blueBackground).resizable()
-        }
+        return homePetModel.petType.backgroundImage
     }
     
     /// 레벨에 맞는 캐릭터 이미지
     func characterImage() -> Image {
-        switch (homePetModel.petType, currentKcalModel.level) {
-        case (.pinkCat, 1):
-            return Image(.pinkEgg).resizable()
-        case (.pinkCat, 2):
-            return Image(.pinkLv1).resizable()
-        case (.pinkCat, 3):
-            return Image(.pinkLv2).resizable()
-        case (.pinkCat, 4):
-            return Image(.pinkLv3).resizable()
-        case (.pinkCat, 5):
-            return Image(.pinkLv4).resizable()
-        case (.greenHam, 1):
-            return Image(.greenEgg).resizable()
-        case (.greenHam, 2):
-            return Image(.greenLv1).resizable()
-        case (.greenHam, 3):
-            return Image(.greenLv2).resizable()
-        case (.greenHam, 4):
-            return Image(.greenLv3).resizable()
-        case (.greenHam, 5):
-            return Image(.greenLv4).resizable()
-        case (.bluePenguin, 1):
-            return Image(.blueEgg).resizable()
-        case (.bluePenguin, 2):
-            return Image(.blueLv1).resizable()
-        case (.bluePenguin, 3):
-            return Image(.blueLv2).resizable()
-        case (.bluePenguin, 4):
-            return Image(.blueLv3).resizable()
-        case (.bluePenguin, 5):
-            return Image(.blueLv4).resizable()
-        case (.purpleDog, 1):
-            return Image(.purpleEgg).resizable()
-        case (.purpleDog, 2):
-            return Image(.purpleLv1).resizable()
-        case (.purpleDog, 3):
-            return Image(.purpleLv2).resizable()
-        case (.purpleDog, 4):
-            return Image(.purpleLv3).resizable()
-        case (.purpleDog, 5):
-            return Image(.purpleLv4).resizable()
-        default:
-            return Image(.pinkEgg).resizable() // Default character
-        }
+        return homePetModel.petType.image(for: homePetModel.level)
     }
     
     @MainActor
