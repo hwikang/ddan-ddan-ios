4038ec678add2b3eabaaf78ce4779f4e50dc1ae7
Zoe <mam07065@naver.com>
2024-11-19 05:04:26 +0900

    스플래쉬 뷰 구현

---
+++ DDanDDan/Presenter/SplashView.swift
@@ -0,0 +1,30 @@
+//
+//  SplashView.swift
+//  DDanDDan
+//
+//  Created by 이지희 on 11/19/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    @StateObject var viewModel: SplashViewModel
+    
+    var body: some View {
+        ZStack {
+            Color(.backgroundBlack)
+                .ignoresSafeArea(.all)
+            VStack(alignment: .center) {
+                Spacer()
+                Image(.splashLogo)
+                Spacer()
+                Image(.splashStart)
+            }
+        }
+        .onAppear {
+            Task {
+                await viewModel.performInitialSetup() // 초기 작업 실행
+            }
+        }
+    }
+}
