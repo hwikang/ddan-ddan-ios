48cb2bf52a564fbdbba8c13ba7c710e3b884e805
Zoe <mam07065@naver.com>
2024-11-20 20:07:36 +0900

    새로운 펫 추가 뷰 구현

---
+++ DDanDDan/Presenter/Home/Views/Challenge/NewPetView.swift
@@ -0,0 +1,45 @@
+//
+//  NewPetView.swift
+//  DDanDDan
+//
+//  Created by 이지희 on 11/20/24.
+//
+
+import SwiftUI
+
+struct NewPetView: View {
+    @ObservedObject var coordinator: AppCoordinator
+
+    var body: some View {
+      ZStack(alignment: .top) {
+        Color(.backgroundBlack)
+        VStack {
+          Spacer()
+          imageView
+          Text("새로운 펫을\n키울 수 있어요!")
+            .multilineTextAlignment(.center)
+            .font(.neoDunggeunmo24)
+            .foregroundStyle(.white)
+            .padding(.vertical, 32)
+          Spacer()
+          GreenButton(action: {
+              coordinator.pop()
+          }, title: "시작하기", disabled: .constant(false))
+          .padding(.bottom, 44)
+        }
+      }
+      .ignoresSafeArea()
+    }
+    
+    var imageView: some View {
+      ZStack {
+        Image(.pangGraphics)
+        Image(.blueLv3)
+          .offset(y: 18)
+      }
+    }
+}
+
+#Preview {
+    NewPetView(coordinator: .init())
+}
