65673a25e233d8ade73c227e2607d8c9f74690e6
Zoe <mam07065@naver.com>
2024-11-21 05:03:40 +0900

    메인 펫 선택 로직 추가

--- DDanDDan/Presenter/Home/Views/Archive/PetArchiveView.swift
+++ DDanDDan/Presenter/Home/Views/Archive/PetArchiveView.swift
@@ -10,14 +10,8 @@ import SwiftUI
 struct PetArchiveView: View {
     @ObservedObject var coordinator: AppCoordinator
     @StateObject var viewModel: PetArchiveViewModel
-    
-    @State private var selectedIndex: Int? = nil
+
     @State private var showToast = false
-    
-    init(viewModel: PetArchiveViewModel, coordinator: AppCoordinator) {
-        self.coordinator = coordinator
-        self._viewModel = StateObject(wrappedValue: viewModel)
-    }
     
     let columns = [
         GridItem(.flexible()),
@@ -54,43 +48,50 @@ struct PetArchiveView: View {
                 .frame(height: 48)
                 .padding(.horizontal, 20)
                 .padding(.bottom, 28)
-                
-                
-                HStack {
-                    LazyVGrid(columns: columns, spacing: 20) {
-                        ForEach(0..<9, id: \.self) { index in
-                            ZStack {
-                                RoundedRectangle(cornerSize: CGSize(width: 8, height: 8))
-                                    .stroke(selectedIndex == index ? Color.buttonGreen : Color.clear, lineWidth: 4)
-                                    .background(
-                                        RoundedRectangle(cornerRadius: 8).foregroundColor(.borderGray) // 배경색 적용
-                                    )
-                                    .frame(maxWidth: 100, maxHeight: 100)
-                                
-                                if index < viewModel.PetList.count {
-                                    viewModel.characterImage(petType: viewModel.PetList[index].type, level: viewModel.PetList[index].level)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
+                    HStack {
+                        LazyVGrid(columns: columns, spacing: 20) {
+                            ForEach(0..<9, id: \.self) { index in
+                                ZStack {
+                                    let pet = viewModel.petList[safe: index]
+                                    
+                                    // UserDefaults에서 저장된 petId와 비교하여 border 색상 설정
+                                    RoundedRectangle(cornerSize: CGSize(width: 8, height: 8))
+                                        .stroke(viewModel.selectedIndex == index ? Color.buttonGreen : Color.clear, lineWidth: 4)
+                                        .background(
+                                            RoundedRectangle(cornerRadius: 8).foregroundColor(.borderGray)
+                                        )
                                         .frame(maxWidth: 100, maxHeight: 100)
-                                } else {
-                                    Image(.questionMark)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 48, height: 48)
-                                        .padding(24)
+                                    
+                                    if let pet = pet {
+                                        pet.type.image(for: pet.level)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(maxWidth: 100, maxHeight: 100)
+                                    } else {
+                                        Image(.questionMark)
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 48, height: 48)
+                                            .padding(24)
+                                    }
                                 }
-                            }
-                            .onTapGesture {
-                                toggleSelection(for: index)
+                                .onTapGesture {
+                                    viewModel.toggleSelection(for: index)
+                                }
                             }
                         }
                     }
-                }
-                .padding(.horizontal, 20)
+                    .padding(.horizontal, 20)
                 Spacer()
                 GreenButton(action: {
-                    print("선택 완료")
-                    showToastMessage()
+                    if !viewModel.petId.isEmpty {
+                        Task {
+                            await viewModel.selectMainPet(id: viewModel.petId)
+                        }
+                        coordinator.pop()
+                    } else {
+                        showToastMessage()
+                    }
                 }, title: "선택 완료", disabled: .constant(false))
                 .padding(.bottom, 44)
             }
@@ -109,14 +110,10 @@ struct PetArchiveView: View {
             }
         }
         .navigationBarHidden(true)
-    }
-    
-
-    private func toggleSelection(for index: Int) {
-        if selectedIndex == index {
-            selectedIndex = nil // 이미 선택된 경우 선택 해제
-        } else {
-            selectedIndex = index // 새로운 인덱스를 선택
+        .onAppear {
+            Task {
+                await viewModel.fetchPetArchive()
+            }
         }
     }
     
@@ -134,5 +131,5 @@ struct PetArchiveView: View {
 
 
 #Preview {
-    PetArchiveView(viewModel: PetArchiveViewModel(repository: HomeRepository()), coordinator: AppCoordinator())
+    PetArchiveView(coordinator: AppCoordinator(), viewModel: PetArchiveViewModel(repository: HomeRepository()))
 }
--- DDanDDan/Presenter/Home/Views/Archive/PetArchiveViewModel.swift
+++ DDanDDan/Presenter/Home/Views/Archive/PetArchiveViewModel.swift
@@ -11,70 +11,53 @@ import HealthKit
 final class PetArchiveViewModel: ObservableObject {
     private let homeRepository: HomeRepositoryProtocol
     
-    @Published var PetList: [Pet] = []
+    @Published var petList: [Pet] = []
+    @Published var selectedIndex: Int? = nil
+    @Published var isLoading: Bool = false // 추가된 상태 변수
+    @Published var petId: String = ""
+    
     
     init(repository: HomeRepositoryProtocol) {
         self.homeRepository = repository
-        Task {
-            await fetchPetArchive()
+    }
+    
+    func setSelectedPet() {
+        for (index, pet) in petList.enumerated() {
+            if pet.id == UserDefaultValue.petId {
+                selectedIndex = index
+                break
+            }
         }
     }
     
-    @MainActor
+    func toggleSelection(for index: Int) {
+        if selectedIndex == index {
+            selectedIndex = nil
+        } else {
+            selectedIndex = index
+        }
+        
+        if let selectedPet = petList[safe: index] {
+            petId = selectedPet.id
+        }
+    }
+    
     func fetchPetArchive() async {
         let petArchiveModel = await homeRepository.getPetArchive()
         
         if case .success(let petArchive) = petArchiveModel {
             UserDefaultValue.userId = petArchive.ownerUserId
-            PetList = petArchive.pets
+            DispatchQueue.main.async {
+                self.petList = petArchive.pets
+            }
         }
     }
     
-    /// 레벨에 맞는 캐릭터 이미지
-    func characterImage(petType: PetType, level: Int) -> Image {
-        switch (petType, level) {
-        case (.pinkCat, 1):
-            return Image(.pinkEgg).resizable()
-        case (.pinkCat, 2):
-            return Image(.pinkLv1).resizable()
-        case (.pinkCat, 3):
-            return Image(.pinkLv2).resizable()
-        case (.pinkCat, 4):
-            return Image(.pinkLv3).resizable()
-        case (.pinkCat, 5):
-            return Image(.pinkLv4).resizable()
-        case (.greenHam, 1):
-            return Image(.greenEgg).resizable()
-        case (.greenHam, 2):
-            return Image(.greenLv1).resizable()
-        case (.greenHam, 3):
-            return Image(.greenLv2).resizable()
-        case (.greenHam, 4):
-            return Image(.greenLv3).resizable()
-        case (.greenHam, 5):
-            return Image(.greenLv4).resizable()
-        case (.bluePenguin, 1):
-            return Image(.blueEgg).resizable()
-        case (.bluePenguin, 2):
-            return Image(.blueLv1).resizable()
-        case (.bluePenguin, 3):
-            return Image(.blueLv2).resizable()
-        case (.bluePenguin, 4):
-            return Image(.blueLv3).resizable()
-        case (.bluePenguin, 5):
-            return Image(.blueLv4).resizable()
-        case (.purpleDog, 1):
-            return Image(.purpleEgg).resizable()
-        case (.purpleDog, 2):
-            return Image(.purpleLv1).resizable()
-        case (.purpleDog, 3):
-            return Image(.purpleLv2).resizable()
-        case (.purpleDog, 4):
-            return Image(.purpleLv3).resizable()
-        case (.purpleDog, 5):
-            return Image(.purpleLv4).resizable()
-        default:
-            return Image(.questionMark).resizable() // Default character
+    func selectMainPet(id: String) async {
+        let result = await homeRepository.updateMainPet(petId: id)
+        if case .success(let pet) = result {
+            UserDefaultValue.petId = pet.mainPet.id
+            UserDefaultValue.petType = pet.mainPet.type.rawValue
         }
     }
 }
