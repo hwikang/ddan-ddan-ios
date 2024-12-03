98c666fc01bc9ebf8b6cc3ce6bba17228649276c
Zoe <mam07065@naver.com>
2024-12-04 03:33:20 +0900

    회원가입 과정 폰트 지정

--- DDanDDan/Presenter/Login/SignUp/SignUpNicknameView.swift
+++ DDanDDan/Presenter/Login/SignUp/SignUpNicknameView.swift
@@ -20,7 +20,7 @@ public struct SignUpNicknameView: View {
                 VStack(alignment: .leading) {
                     
                     Text("별명을 알려주세요")
-                        .font(.system(size: 24, weight: .bold))
+                        .font(.neoDunggeunmo24)
                         .foregroundStyle(.white)
                         .padding(.top, 80)
                     
