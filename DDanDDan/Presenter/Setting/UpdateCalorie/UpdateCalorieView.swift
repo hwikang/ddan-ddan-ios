b9a50195b9b51b85c67494de74af1d8b92d7ee8e
Zoe <mam07065@naver.com>
2024-12-10 03:52:01 +0900

    칼로리 변경 시 토스트 메시지 표출

--- DDanDDan/Presenter/Setting/UpdateCalorie/UpdateCalorieView.swift
+++ DDanDDan/Presenter/Setting/UpdateCalorie/UpdateCalorieView.swift
@@ -62,7 +62,17 @@ struct UpdateCalorieView: View {
                             
                 }, title: "변경 완료", disabled: .constant(false))
             }
-            
+            TransparentOverlayView(isPresented: $viewModel.showToast) {
+                VStack {
+                    ToastView(message: viewModel.toastMessage)
+                }
+                .transition(.asymmetric(
+                    insertion: .move(edge: .top).combined(with: .opacity),
+                    removal: .opacity))
+                .animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3), value: viewModel.showToast)
+                .position(x: UIScreen.main.bounds.width / 2 + 10, y: UIScreen.main.bounds.height - 250)
+            }
+
         }
         .navigationBarHidden(true)
     }
--- DDanDDan/Presenter/Setting/UpdateCalorie/UpdateCalorieViewModel.swift
+++ DDanDDan/Presenter/Setting/UpdateCalorie/UpdateCalorieViewModel.swift
@@ -12,10 +12,12 @@ public protocol UpdateCalorieViewModelProtocol: ObservableObject {
 }
 
 final class UpdateCalorieViewModel: UpdateCalorieViewModelProtocol {
-  
+    
+    @Published var showToast: Bool = false
+    @Published var toastMessage: String = ""
     @Published var calorie: Int = 100
     private let repository: SettingRepositoryProtocol
-
+    
     init(repository: SettingRepositoryProtocol) {
         self.repository = repository
         calorie =  UserDefaultValue.purposeKcal
@@ -27,6 +29,7 @@ final class UpdateCalorieViewModel: UpdateCalorieViewModelProtocol {
                                              purposeCalorie: calorie)
         switch result {
         case .success:
+            showToastMessage(message: "하루 목표 칼로리가 변경되었어요")
             return true
         case .failure(let failure):
             //TODO: 에러처리
@@ -35,12 +38,30 @@ final class UpdateCalorieViewModel: UpdateCalorieViewModelProtocol {
     }
     
     public func increaseCalorie() {
-        guard calorie < 1000 else { return }
+        guard calorie < 1000 else {
+            showToastMessage(message: "최대 1000칼로리까지 성장할 수 있어요")
+            return
+        }
         calorie += 100
     }
     
     public func decreaseCalorie() {
-        guard calorie > 100 else { return }
+        guard calorie > 100 else {
+            showToastMessage(message: "최소 100칼로리 이상 설정해주세요")
+            return
+        }
         calorie -= 100
+    }
+    
+    private func showToastMessage(message: String) {
+        toastMessage = message
+        showToast = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            self?.hideToastMessage()
+        }
+    }
+    
+    private func hideToastMessage() {
+        showToast = false
     }
 }
